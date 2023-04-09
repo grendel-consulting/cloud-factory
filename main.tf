522941fc304d640db5a5a838b0017a85c9165a45
James Ramirez <james.ramirez@grendel-consulting.com>
2023-04-09 09:02:19 +0100

    feat: switch sub-states to use Terraform Cloud and latest terraform

--- main.tf
+++ main.tf
@@ -1,6 +1,13 @@
 module "factory" {
   source = "github.com/grendel-consulting/terraform-aws-control_tower_account_factory"
 
+  aft_framework_repo_url     = "https://github.com/grendel-consulting/terraform-aws-control_tower_account_factory.git"
+  aft_framework_repo_git_ref = "main"
+  terraform_distribution     = "tfc"
+  terraform_org_name         = "grendel-consulting"
+  terraform_token            = var.terraform_token
+  terraform_version          = "1.4.4"
+
   ct_management_account_id    = var.ct_management_account_id
   log_archive_account_id      = var.log_archive_account_id
   audit_account_id            = var.audit_account_id
@@ -15,5 +22,5 @@ module "factory" {
   account_customizations_repo_name              = "${var.github_owner}/cloud-factory-customisations"
 
   aft_feature_delete_default_vpcs_enabled = true
-  aft_vpc_endpoints = false
+  aft_vpc_endpoints                       = false
 }
